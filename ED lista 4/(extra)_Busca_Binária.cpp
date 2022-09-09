50974ed8e0c727520aba131be5d663c62aa287c7
Luis Ferlim Lima <101893830+Luisferlim@users.noreply.github.com>
2022-09-08 23:53:11 -0300

    Add files via upload

--- ED lista 4/(extra)_Busca_Binária.cpp
+++ ED lista 4/(extra)_Busca_Binária.cpp
@@ -6,11 +6,12 @@ using namespace std;
 int busca_binaria(int *vect, int size, int item)
 {
     int begin = 0;
-    int end = size - 1; //respectivamento o comeco e o final da busca
-
-    while (begin <= end) //condicao de parada da busca
+    int end = size; //respectivamento o comeco e o final da busca
+    int i;
+    
+    while (begin < end) //condicao de parada da busca
     {
-        int i = (begin + end)/2;
+        i = (begin + end)/2;
 
         if (vect[i] == item) //achou!
         {
@@ -22,6 +23,7 @@ int busca_binaria(int *vect, int size, int item)
             begin = i+1; //item, se existir, estara a direita de i
         }
         else
+            if(vect[i] > item)
         {
             end = i; //item, se existir, estara a esquerda de i
         }
@@ -31,10 +33,17 @@ int busca_binaria(int *vect, int size, int item)
 
 int main()
 {
-    int *vector, size;  
+    int *vector, size, item;
+
+    cout << "digite o tamanho do vetor: ";
     cin >> size;
     vector = new int(size); // alocacao dinamica
-    
-    for(int i =0; i < size; i++) {cout << vector[i] << ", ";}
+    cout << "digite os elementos do vetor: ";
+    for(int i =0; i < size; i++) {cin >> vector[i];}//for de uma linha para preencher o vetor
+
+    cout << "digite o elemento a ser procurado: ";
+    cin >> item;
+
+    cout << busca_binaria(vector, size, item);//mostra o retorno da binary_search
     return 0;
 }
